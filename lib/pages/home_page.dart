5ac6a1e8722cb57f82a5853082e03cbe51f2d689
ccbaide86 <ccbaide86@gmail.com>
2023-07-18 18:07:42 -0600

    Second Commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,17 +10,22 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CircleAvatar(
+    return Scaffold(
+      appBar: AppBar(
+      backgroundColor: Colors.white, 
+      elevation: 0,
+      leading: const Icon(Icons.arrow_left, color: Colors.black, size: 50),
+),
+      body: const Align(
+      alignment: Alignment.topCenter,
+      child: 
+        CircleAvatar(
+          radius: 70,
+          backgroundColor: Colors.pinkAccent,
+          child: Text('P',style: TextStyle(fontSize: 60, color: Colors.white),
+          ),
         ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          //Icon(Icon.Call),
-        ],
-      ),
+),
     );
   }
 }
